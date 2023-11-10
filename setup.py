ac8ab0b7cd08b75fe8145cdda4020798a532583f
Markus Winkler <winkler.info@icloud.com>
2023-11-10 20:48:59 +0100

    release 0.42.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="pyastroweatherio",
     packages=["pyastroweatherio"],
-    version="0.42.2.dev5",
+    version="0.42.2",
     license="MIT",
     description="Python Wrapper for 7Timer and Met.no REST API",
     long_description=" ".join(
